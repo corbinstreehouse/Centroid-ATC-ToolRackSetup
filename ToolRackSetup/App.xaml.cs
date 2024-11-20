6ce6e5f665069499ee32142445b1b54e6ceca8ea
corbin dunn <corbin@corbinstreehouse.com>
2024-11-20 08:06:16 -0800

    Hardcode assembly resolve to c:\cncm...I can't figure out how to get the app config stuff to work witht his version of .net

--- ToolRackSetup/App.xaml.cs
+++ ToolRackSetup/App.xaml.cs
@@ -1,5 +1,7 @@
 ﻿using System.Configuration;
 using System.Data;
+using System.IO;
+using System.Reflection;
 using System.Windows;
 using System.Windows.Threading;
 using WpfSingleInstanceByEventWaitHandle;
@@ -9,9 +11,38 @@ namespace ToolRackSetup
     /// <summary>
     /// Interaction logic for App.xaml
     /// </summary>
+    /// 
+
+
+
     public partial class App : Application
     {
-        
+
+        private static Assembly CurrentDomain_AssemblyResolve(object? sender, ResolveEventArgs args)
+        {
+            // Extract the assembly name
+            string assemblyName = new AssemblyName(args.Name).Name;
+
+            // Define the path to the assembly
+            string assemblyPath = Path.Combine("C:\\CNCM", $"{assemblyName}.dll");
+
+            // Load and return the assembly
+            if (File.Exists(assemblyPath))
+            {
+                return Assembly.LoadFrom(assemblyPath);
+            }
+
+            // Return null if the assembly cannot be found
+            return null;
+        }
+
+
+        public App() : base() 
+        {
+                AppDomain.CurrentDomain.AssemblyResolve += new ResolveEventHandler(CurrentDomain_AssemblyResolve);
+        }
+
+
         protected override void OnStartup(StartupEventArgs e)
         {
             WpfSingleInstance.Make("ToolRackSetup");
