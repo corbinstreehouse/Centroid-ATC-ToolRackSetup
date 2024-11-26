4cae0c745c536610cab9ec7e6602f64944a218a1
corbin dunn <corbin@corbinstreehouse.com>
2024-11-26 07:49:19 -0800

    Theme WIP

--- ToolRackSetup/MainWindow.xaml.cs
+++ ToolRackSetup/MainWindow.xaml.cs
@@ -26,6 +26,7 @@ using static CentroidAPI.CNCPipe;
 using static CentroidAPI.CNCPipe.State;
 using System.Linq;
 using CommunityToolkit.Mvvm.ComponentModel;
+using System.Windows.Controls.Primitives;
 
 namespace ToolRackSetup
 {
@@ -456,11 +457,64 @@ namespace ToolRackSetup
         public ParameterSettings _parameterSettings { get; }
 
         bool _loading = true;
+
+        private string GetThemeFilename()
+        {
+            string fileContents = File.ReadAllText("c:\\cncm\\ColorPicker.txt");
+            fileContents = fileContents.Trim();
+            if (!File.Exists(fileContents))
+            {
+                // add the resource directory
+                return String.Format("C:\\cncm\\resources\\colors\\{0}", fileContents);
+            }
+            return fileContents;
+        }
+        public System.Windows.Media.Color ColorFromString(String hex)
+        {
+            byte a = 255;
+            byte r = 255;
+            byte g = 255;
+            byte b = 255;
+            int start = 0;
+            //handle ARGB strings (8 characters long)
+            if (hex.Length == 8)
+            {
+                a = byte.Parse(hex.Substring(0, 2), System.Globalization.NumberStyles.HexNumber);
+                start = 2;
+            }
+            //convert RGB characters to bytes
+            r = byte.Parse(hex.Substring(start, 2), System.Globalization.NumberStyles.HexNumber);
+            g = byte.Parse(hex.Substring(start + 2, 2), System.Globalization.NumberStyles.HexNumber);
+            b = byte.Parse(hex.Substring(start + 4, 2), System.Globalization.NumberStyles.HexNumber);
+            return System.Windows.Media.Color.FromArgb(a, r, g, b);
+        }
+        private void LoadTheme()
+        {
+            string filename = GetThemeFilename();
+            XDocument doc = XDocument.Load(filename);
+            Color GetThemeColor(string name)
+            {
+                string key = String.Format("/Table/Colors/{0}", name);
+                string value = doc.XPathSelectElement(key)!.Value;
+                return ColorFromString(value);
+            }
+
+         //   Color bgColor = GetThemeColor("backgroundColor");
+         //   this.Background = new SolidColorBrush(bgColor);
+            // TODO: load/set other colors, but use the resources way..
+            //if (Application.Current.Resources.Contains("txtColor"))
+            //{
+            //    Application.Current.Resources["txtColor"] = new SolidColorBrush(Colors.Blue);
+            //}
+
+
+        }
         public MainWindow()
         {
 
             InitializeComponent();
 
+            LoadTheme();
             // Variables for the API Connection MessageBox
             var messageBoxText = "Can't connect to CNC12. Make sure it is running! Would you like to retry the connection?";
             var messageBoxTitle = "Error Connecting to CNC12!";
