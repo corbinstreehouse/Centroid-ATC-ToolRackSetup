0960ae9886a524b98961198ac78206fcf188bc9a
corbin dunn <corbin@corbinstreehouse.com>
2024-11-26 20:47:14 -0800

    ClickSelectTestBox to its own file

---
+++ ToolRackSetup/ClickSelectTextBox.cs
@@ -0,0 +1,99 @@
+﻿using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Security.Cryptography;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using CentroidAPI;
+using System.Xml.Linq;
+using System.IO;
+using static CentroidAPI.CNCPipe.Tool;
+using System.Xml;
+using System.Xml.XPath;
+using System.Globalization;
+using System.Runtime;
+using System.Linq.Expressions;
+using static CentroidAPI.CNCPipe;
+using static CentroidAPI.CNCPipe.State;
+using System.Linq;
+using CommunityToolkit.Mvvm.ComponentModel;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Security.Cryptography;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using CentroidAPI;
+using System.Xml.Linq;
+using System.IO;
+using System.Xml;
+using System.Xml.XPath;
+using System.Globalization;
+using System.Runtime;
+
+namespace ToolRackSetup
+{
+
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    /// 
+    public class ClickSelectTextBox : TextBox
+    {
+        public ClickSelectTextBox()
+        {
+            AddHandler(PreviewMouseLeftButtonDownEvent,
+              new MouseButtonEventHandler(SelectivelyIgnoreMouseButton), true);
+            AddHandler(GotKeyboardFocusEvent,
+              new RoutedEventHandler(SelectAllText), true);
+            AddHandler(MouseDoubleClickEvent,
+              new RoutedEventHandler(SelectAllText), true);
+        }
+
+        private static void SelectivelyIgnoreMouseButton(object sender,
+                                                         MouseButtonEventArgs e)
+        {
+            // Find the TextBox
+            DependencyObject parent = e.OriginalSource as UIElement;
+            while (parent != null && !(parent is TextBox))
+                parent = VisualTreeHelper.GetParent(parent);
+
+            if (parent != null)
+            {
+                var textBox = (TextBox)parent;
+                if (!textBox.IsKeyboardFocusWithin)
+                {
+                    // If the text box is not yet focussed, give it the focus and
+                    // stop further processing of this click event.
+                    textBox.Focus();
+                    e.Handled = true;
+                }
+            }
+        }
+
+        private static void SelectAllText(object sender, RoutedEventArgs e)
+        {
+            var textBox = e.OriginalSource as TextBox;
+            if (textBox != null)
+                textBox.SelectAll();
+        }
+    }
+}
--- ToolRackSetup/MainWindow.xaml.cs
+++ ToolRackSetup/MainWindow.xaml.cs
@@ -26,8 +26,6 @@ using static CentroidAPI.CNCPipe;
 using static CentroidAPI.CNCPipe.State;
 using System.Linq;
 using CommunityToolkit.Mvvm.ComponentModel;
-using System.Windows.Controls.Primitives;
-using System.Runtime.Serialization;
 
 namespace ToolRackSetup
 {
@@ -86,50 +84,7 @@ namespace ToolRackSetup
         #endregion
     }
 
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
-    /// 
-    public class ClickSelectTextBox : TextBox
-    {
-        public ClickSelectTextBox()
-        {
-            AddHandler(PreviewMouseLeftButtonDownEvent,
-              new MouseButtonEventHandler(SelectivelyIgnoreMouseButton), true);
-            AddHandler(GotKeyboardFocusEvent,
-              new RoutedEventHandler(SelectAllText), true);
-            AddHandler(MouseDoubleClickEvent,
-              new RoutedEventHandler(SelectAllText), true);
-        }
-
-        private static void SelectivelyIgnoreMouseButton(object sender,
-                                                         MouseButtonEventArgs e)
-        {
-            // Find the TextBox
-            DependencyObject parent = e.OriginalSource as UIElement;
-            while (parent != null && !(parent is TextBox))
-                parent = VisualTreeHelper.GetParent(parent);
-
-            if (parent != null)
-            {
-                var textBox = (TextBox)parent;
-                if (!textBox.IsKeyboardFocusWithin)
-                {
-                    // If the text box is not yet focussed, give it the focus and
-                    // stop further processing of this click event.
-                    textBox.Focus();
-                    e.Handled = true;
-                }
-            }
-        }
-
-        private static void SelectAllText(object sender, RoutedEventArgs e)
-        {
-            var textBox = e.OriginalSource as TextBox;
-            if (textBox != null)
-                textBox.SelectAll();
-        }
-    }
+   
     public class MyPropertyChangedEventArgs: PropertyChangedEventArgs
     {
         public Object? oldValue { get; }
