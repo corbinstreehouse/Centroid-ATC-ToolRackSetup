2951c130c1dd4cc1762c5ec859711c7e0fb79e20
corbin dunn <corbin@corbinstreehouse.com>
2024-12-12 10:48:04 -0800

    New keep alive window

---
+++ ToolRackSetup/KeepAliveWindow.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace ToolRackSetup
+{
+    /// <summary>
+    /// Interaction logic for KeepAliveWindow.xaml
+    /// </summary>
+    public partial class KeepAliveWindow : Window
+    {
+        public KeepAliveWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            ((App)App.Current).ShowToolManagerWindow();
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            System.Environment.Exit(0);
+        }
+    }
+}
